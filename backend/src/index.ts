b41a7b73c1e748c0cc20bacca95db729cec86358
Nishant <nishantkumardash26@gmail.com>
2024-03-02 23:40:38 +0530

    blog

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,27 +12,5 @@ const app = new Hono<{
 
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
-app.get("/api/v1/auth/blog/:id", (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("get blog route");
-});
-
-app.post("/api/v1/auth/blog", (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  return c.text("signin route");
-});
-
-app.put("/api/v1/auth/blog", (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  return c.text("signin route");
-});
 
 export default app;
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,5 +1,119 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
 
-const blogRouter = new Hono();
+const blogRouter = new Hono<{
+  Bindings: {
+    JWT_SECRET: string;
+    DATABASE_URL: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
 
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("Authorization") || "";
+
+  const token = authHeader?.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (payload) {
+    c.set("userId", payload.id);
+    await next();
+  } else return c.json({ error: "Unauthorized access" });
+});
+
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const blogs = await prisma.blog.findMany();
+    return c.json({ blogs: blogs });
+  } catch (e) {
+    console.log(e);
+    return c.json("Internal Server Error");
+  }
+});
+blogRouter.get("/:id", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const id = c.req.param("id");
+
+  try {
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (blog) {
+      return c.json({ blog: blog });
+    } else return c.json({ error: `Unable to find the blog with id : ${id}` });
+  } catch (e) {
+    console.log(e);
+    return c.json({ error: "Internal server error" });
+  }
+});
+
+blogRouter.post("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const userId = c.get("userId");
+
+    const blogBody = await c.req.json();
+    const newBlog = prisma.blog.create({
+      data: {
+        title: blogBody.title,
+        content: blogBody.content,
+        userId: userId,
+      },
+    });
+    return c.json({ message: `Blog created with id : ${(await newBlog).id}` });
+  } catch (e) {
+    console.log(e);
+    c.json({ error: "Internal Server Error" });
+  }
+});
+
+blogRouter.put("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const body = await c.req.json();
+    const blog = await prisma.blog.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({ message: "Blog updated" });
+  } catch (e) {
+    console.log(e);
+    return c.json("Internal Server Error");
+  }
+});
+
+// blogRouter.get("/bulk", async (c) => {
+//   const prisma = new PrismaClient({
+//     datasourceUrl: c.env.DATABASE_URL,
+//   }).$extends(withAccelerate());
+//   try {
+//     const blogs = await prisma.blog.findMany();
+//     return c.json({ blogs: blogs });
+//   } catch (e) {
+//     console.log(e);
+//     return c.json("Internal Server Error");
+//   }
+// });
+
+//moving before :id as control first reaches this first as :id is getting bulk
 export { blogRouter };
